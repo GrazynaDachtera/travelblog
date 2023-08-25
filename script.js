3117cca5167fb106cb18af608bdc07d2094a8163
Grażyna Dachtera <grazynadachtera@gmail.com>
2023-08-25 22:31:16 +0200

    added changes in navigation bar

--- script.js
+++ script.js
@@ -2,11 +2,20 @@
 window.addEventListener("scroll", () => {
     const headline = document.querySelector(".headline");
     const scrollTop = window.scrollY;
-    const maxScroll = 100; // Adjust this value as needed
+    const maxScroll = 100;
 
-    // Calculate the opacity based on scroll position
     const opacity = 1 - (scrollTop / maxScroll);
 
-    // Apply the opacity to the menu
     headline.style.opacity = opacity < 0 ? 0 : opacity;
 });
+
+const ofertaMenuItem = document.querySelector('.menu .dropdown > a'); // Target the anchor tag
+
+const submenu = ofertaMenuItem.nextElementSibling; // Use nextElementSibling to target the ul element
+
+ofertaMenuItem.addEventListener('click', function(event) {
+    event.preventDefault();
+    submenu.style.display = (submenu.style.display === 'block') ? 'none' : 'block';
+});
+
+
